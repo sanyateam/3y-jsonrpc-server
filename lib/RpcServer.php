b5e34ecaa74301de2c2f6ed88408c38b80b289c6
chaz6chez <250220719@qq.com>
2020-11-13 15:13:47 +0800

    更新debug模式 新增serviceErrorException 新增业务抛出

--- lib/RpcServer.php
+++ lib/RpcServer.php
@@ -5,6 +5,7 @@ namespace JsonRpcServer;
 use JsonRpcServer\Exception\MethodNotFoundException;
 use JsonRpcServer\Exception\RpcException;
 use JsonRpcServer\Exception\ServerErrorException;
+use JsonRpcServer\Exception\ServiceErrorException;
 use JsonRpcServer\Format\ErrorFmt;
 use JsonRpcServer\Format\JsonFmt;
 use Workerman\Connection\TcpConnection;
@@ -25,6 +26,7 @@ class RpcServer extends Worker {
      */
     protected static $_registered = [];
     protected static $_params     = null;
+    protected static $_debug      = false;
     /**
      * @var TcpConnection
      */
@@ -40,23 +42,26 @@ class RpcServer extends Worker {
         list(, $address) = \explode(':', $socket_name, 2);
         parent::__construct("JsonRpc2:{$address}", $context_option);
         $this->name = 'JsonRpcServer';
-
-    }
-
-    public static function setConnection(TcpConnection $connection){
-        self::$_connection = $connection;
     }
 
     public static function getConnection(){
         return self::$_connection;
     }
 
-    public static function setParams($params){
-        self::$_params = $params;
-    }
-
     public static function getParams(){
         return self::$_params;
+    }
+
+    public static function setDebug(bool $debug = false){
+        self::$_debug = $debug;
+    }
+
+    public static function getDebug(){
+        return self::$_debug;
+    }
+
+    public static function throwServiceExcption($info, $message = 'Internal error'){
+        return new ServiceErrorException($message, null, $info);
     }
 
     /**
@@ -84,6 +89,7 @@ class RpcServer extends Worker {
     }
     public function onConnect(TcpConnection $connection){
         self::safeEcho("\n# ------ {$this->_pid} CONNECT [{$connection->id}] ------ #\n");
+        self::$_connection = $connection;
     }
     public function onClose(TcpConnection $connection){
         self::safeEcho("\n# ------ {$this->_pid} CLOSE [{$connection->id}] ------ #\n");
@@ -106,52 +112,32 @@ class RpcServer extends Worker {
     public function onMessage(TcpConnection $connection, $data) {
         list($exception, $buffer) = $data;
         $fmt = JsonFmt::factory($buffer);
-        self::setConnection($connection);
-        self::setParams($fmt->params);
-        self::safeEcho("\n <recv>: {$GLOBALS['recv_buffer']}");
+        self::$_params = $fmt->params;
 
+        self::safeEcho("\n <recv>: {$GLOBALS['recv_buffer']}");
         $resFmt = clone $fmt;
         $resFmt->clean(true);
         $resFmt->id = $fmt->id ? $fmt->id : null;
         $errorFmt = ErrorFmt::factory();
         # 异常检查
         if($exception instanceof RpcException){
-            $errorFmt->code = $exception->getCode();
-            $errorFmt->message = $exception->getMessage();
-            $resFmt->error = $errorFmt->outputArray($errorFmt::FILTER_STRICT);
-            return $this->_send($connection,$resFmt->outputArrayByKey($resFmt::FILTER_STRICT, $resFmt::TYPE_RESPONSE));
+            return $this->_throwError($exception);
         }
         $e = new ServerErrorException();
         if($exception instanceof \Exception){
-            $errorFmt->code    = $e->getCode();
-            $errorFmt->message = $e->getMessage();
-            $errorFmt->data    = $exception->getTraceAsString();
-            $resFmt->error     = $errorFmt->outputArray($errorFmt::FILTER_STRICT);
-            return $this->_send($connection,$resFmt->outputArrayByKey($resFmt::FILTER_STRICT, $resFmt::TYPE_RESPONSE));
+            return $this->_throwError($e, $exception);
         }
-
-        $class  = explode('.', $fmt->method);
-        $service = $class[0];
-        $method = array_pop($class);
-        $class  = implode('\\', $class);
+        list($class, $method, $service) = $this->_analyzeMethod($fmt->method);
         # 不存在
         if(
             !class_exists($class) or
-            !method_exists($class,$method)
+            !method_exists($class, $method)
         ){
-            $e = new MethodNotFoundException();
-            $errorFmt->code    = $e->getCode();
-            $errorFmt->message = $e->getMessage();
-            $resFmt->error     = $errorFmt->outputArray($errorFmt::FILTER_STRICT);
-            return $this->_send($connection,$resFmt->outputArrayByKey($resFmt::FILTER_STRICT, $resFmt::TYPE_RESPONSE));
+            return $this->_throwError(new MethodNotFoundException(), 'NOT FOUND');
         }
         # 非允许
         if(!in_array($service, $this->_allow)){
-            $e = new MethodNotFoundException();
-            $errorFmt->code    = $e->getCode();
-            $errorFmt->message = $e->getMessage() . '[ALLOW]';
-            $resFmt->error     = $errorFmt->outputArray($errorFmt::FILTER_STRICT);
-            return $this->_send($connection,$resFmt->outputArrayByKey($resFmt::FILTER_STRICT, $resFmt::TYPE_RESPONSE));
+            return $this->_throwError(new MethodNotFoundException(), 'ALLOW');
         }
         # 调用
         try {
@@ -159,13 +145,7 @@ class RpcServer extends Worker {
             $resFmt->result = null;
             # failed
             if(!$resFmt->result = call_user_func_array([$class, $method], [$fmt->params, $this])){
-
-                $errorFmt->code    = $e->getCode();
-                $errorFmt->message = $e->getMessage();
-                $params            = json_encode($fmt->params,JSON_UNESCAPED_UNICODE);
-                $errorFmt->data    = "{$fmt->method} error:{$params}";
-                $resFmt->error     = $errorFmt->outputArray($errorFmt::FILTER_STRICT);
-                return $this->_send($connection,$resFmt->outputArrayByKey($resFmt::FILTER_STRICT, $resFmt::TYPE_RESPONSE));
+                return $this->_throwError($e, 'FUNC');
             }
             # success
             if($fmt->id){
@@ -173,14 +153,7 @@ class RpcServer extends Worker {
             }
             return $this->_send($connection,null);
         } catch(\Exception $exception) {
-            if($exception->getCode() !== '-1'){
-                throw $exception;
-            }
-            $errorFmt->code    = $e->getCode();
-            $errorFmt->message = $e->getMessage();
-            $errorFmt->data    = $exception->getTraceAsString();
-            $resFmt->error     = $errorFmt->outputArray($errorFmt::FILTER_STRICT);
-            return $this->_send($connection,$resFmt->outputArrayByKey($resFmt::FILTER_STRICT, $resFmt::TYPE_RESPONSE));
+            return $this->_throwError($e, $exception);
         }
 
     }
@@ -229,6 +202,26 @@ class RpcServer extends Worker {
         throw new \Exception("class {$class} not found", '-1');
     }
 
+    protected function _analyzeMethod(string $method){
+        $class  = explode('.', $method);
+        $service = $class[0];
+        $method = array_pop($class);
+        $class  = implode('\\', $class);
+        return [$class, $method, $service];
+    }
+
+    protected function _throwError(RpcException $exception, $info = null){
+        $resFmt = JsonFmt::factory();
+        $errorFmt = ErrorFmt::factory();
+        $errorFmt->code    = $exception->getCode();
+        $errorFmt->message = $exception->getMessage();
+        if($info){
+            $errorFmt->data = $info instanceof \Exception ? $this->_debugInfo($info) : $info;
+        }
+        $resFmt->error   = $errorFmt->outputArray($errorFmt::FILTER_STRICT);
+        return $this->_send(self::getConnection(), $resFmt->outputArrayByKey($resFmt::FILTER_STRICT, $resFmt::TYPE_RESPONSE));
+    }
+
     /**
      * @param TcpConnection $connection
      * @param $buffer
@@ -238,5 +231,22 @@ class RpcServer extends Worker {
         $GLOBALS['send_buffer'] = $buffer === null ? "\n" : json_encode($buffer) . "\n";
         self::safeEcho("\n <send>: {$GLOBALS['send_buffer']}");
         return $connection->send($buffer);
+    }
+
+    protected function _debugInfo(\Exception $exception){
+        if(self::$_debug){
+            return [
+                '_code'    => $exception->getCode(),
+                '_message' => $exception->getMessage(),
+                '_file'    => $exception->getFile() . '(' .$exception->getLine(). ')',
+                '_trace'   => $exception->getTraceAsString(),
+                '_info'    => $exception instanceof ServiceErrorException ? $exception->getInfo() : []
+            ];
+        }
+
+        return [
+            '_code'    => $exception->getCode(),
+            '_message' => $exception->getMessage(),
+        ];
     }
 }
